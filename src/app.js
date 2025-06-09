7a7edb8044cb502bc2e1160796f649194540b219
101Vaibhav04 <mc230041036@iiti.ac.in>
2025-06-10 00:11:10 +0530

    feat: implement user registration route and asyncHandler utility; refactor app structure

--- src/app.js
+++ src/app.js
@@ -7,14 +7,12 @@ const app= express();
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
   credentials: true
-}));
+}))
 
-app.use(express.json({limit: '16kb'}));
 
-app.use(expressurlencoded({extended: true, limit: '16kb'}));
+//routes
+import userRouter from './routes/user.routes.js';
+app.use("/api/v1/users", userRouter);
 
-app.use(express.static('public'));  
-
-app.use(cookieParser());
-
+//routes declaration
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
-
+import {app} from './app.js';
 
 import mongoose from 'mongoose';
 import { DB_NAME } from './constants.js';    // Added .js extension
