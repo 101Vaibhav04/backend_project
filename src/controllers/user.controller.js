9265329d6c0304777190b35dd0e267333c29b6c9
101Vaibhav04 <mc230041036@iiti.ac.in>
2025-06-10 13:01:35 +0530

    feat: enhance user registration with validation and error handling; refactor routes and ApiError export

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,48 @@
 import { asyncHandler } from "../utils/asyncHandler.js";   
 
+
+import {ApiError} from "../utils/ApiError.js"; // Import ApiError for error handling
+
+import { User } from "../models/user.model.js"; // Import User model for database operations
+
+
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message:"ok"
-  })
+ //get user details from frontend
+ // validations like not empty
+ //check if user already exixts: username, email
+//check for images , check foravatar
+//upload on cloudinary, avatar
+// create user object - create enry in db
+// remove password and refresh token field from response
+// check for user creation
+//return response   
+
+
+const {fullname, email, username, password}=  req.body
+
+if( 
+  [fullname, email, username, password].some(field => field?.trim() === '')
+) {
+    throw new ApiError(400, "All fields are required")
+}
+
+// Check if user already exists
+const existedUser =  User.findOne({
+  $or: [{username},    {email}]
+})
+
+if(existedUser) {
+  throw new ApiError(400, "User already exists with this username or email")
+}
+
+const avatarLocalPath= req.files?. avatar[0]?.path;
+const coverImageLocalPath= req.files?.coverImage[0]?.path;
+
+if(!avatarLocalPath){
+  throw new ApiError(400, "Avatar image is required");
+}
+
+
 })
 
 
